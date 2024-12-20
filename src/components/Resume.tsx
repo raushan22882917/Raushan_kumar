71ed0fb56373ac57fb68936a20352b231cd78042
Raushan Kumar <121452343+raushan22882917@users.noreply.github.com>
2024-12-20 10:44:24 +0530

    Update Resume.tsx

--- src/components/Resume.tsx
+++ src/components/Resume.tsx
@@ -7,7 +7,7 @@ export const Resume = () => {
       <Button 
         variant="outline" 
         className="group"
-        onClick={() => window.open("https://drive.google.com/file/d/1SL7LlAHNuXnW0Ps-mGHbwMdfexvEhqdA/view?usp=sharing", "_blank")}
+        onClick={() => window.open("https://drive.google.com/file/d/1rmwWRnt4UOseaBoB5xVk0EEt4EZRkc_T/view?usp=sharing", "_blank")}
       >
         <FileDown className="mr-2 h-4 w-4 group-hover:animate-bounce" />
         Download Resume
